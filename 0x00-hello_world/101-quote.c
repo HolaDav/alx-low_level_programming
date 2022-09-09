5bd2810bb96a320ef87271d470aa40f8e16c27ce
HolaDav <oladejidavid12@gmail.com>
2022-09-09 14:02:20 -0700

    tast last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main -Pring a text
  *
- * Return:always(1) Success
+ * Return: always (1) Success
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }

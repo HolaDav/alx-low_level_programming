0bff3eaf039fff3f3ae149a07d37194d8ece4faf
HolaDav <oladejidavid12@gmail.com>
2022-09-09 11:29:18 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print string using puts function
+ *
+ * Return: 0 after printing the function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

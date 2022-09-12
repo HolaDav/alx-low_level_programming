c4f92cc6bcd9ecdd10c8f25627a6966c3847d368
HolaDav <oladejidavid12@gmail.com>
2022-09-12 10:29:15 -0700

    Variable Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
-#include <studlib.h>
-#include <timee.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -12,17 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n % 10 > 5)
+	if (n > 0)
 	{
-		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+		printf("%i is positive\n", n);
 	}
-	else if (n % 10 == 0)
+	else if (n == 0)
 	{
-		printf("Last digit of %i is %i and is 0\n", n, n % 10);
+		printf("%i is zero\n", n);
 	}
 	else
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+		printf("%i is negative\n", n);
 	}
 	return (0);
 }

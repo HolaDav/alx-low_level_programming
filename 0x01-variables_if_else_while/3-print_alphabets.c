fc5e66e607c32c2f6d49be3fdb06d997b384ddbd
HolaDav <oladejidavid12@gmail.com>
2022-09-12 17:01:51 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	char lc;
+	char uc;
+
+	for(lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for(uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	putchar('\n');
+	return (0);
+}

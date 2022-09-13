9f47e495db48b19f838714081174697485cdf34e
HolaDav <oladejidavid12@gmail.com>
2022-09-12 17:46:19 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return 0;
+}

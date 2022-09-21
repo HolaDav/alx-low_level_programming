27c2092b816394abe0e84f623afe251adbf70034
HolaDav <oladejidavid12@gmail.com>
2022-09-21 17:51:03 +0000

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[5];
+	
+	a[0] = 98;
+	a[1] = 402;
+	a[2] = -198;
+	a[3] = 298;
+	a[4] = -1024;
+	print_array(int *a, int n);
+	return (0);
+}

abc6a51c370c6484fe02ec4c3c6496c8beef71de
HolaDav <oladejidavid12@gmail.com>
2022-09-21 05:17:39 +0000

    outs function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * _puts - print a string followed by a new line
  * @str: string variable
@@ -9,6 +10,9 @@
 
 void _puts(char *str)
 {
-	_putchar(str);
-	_putchar("\n");
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
 }

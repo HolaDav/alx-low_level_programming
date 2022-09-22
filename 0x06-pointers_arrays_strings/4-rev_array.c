3299be7229d1d31e240bbc39f5230c9eb2d05fa5
HolaDav <oladejidavid12@gmail.com>
2022-09-22 23:19:32 +0000

    Lowercase to Uppercase

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -17,7 +17,7 @@ void reverse_array(int *a, int n)
 	{
 		p++;
 	}
-	for (k = 0; k < n / 2; k++)
+	for (k = 0; k <= i / 2; k++)
 	{
 		aux = a[k];
 		a[k] = *p;
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - converting any string to upper casw
+ * @str: parameter
+ *
+ * Return: string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}

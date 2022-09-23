398be056f62aed00da1c8c4ae4b9983a0d967c47
HolaDav <oladejidavid12@gmail.com>
2022-09-23 00:46:48 +0000

    Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cao_string - capitalizes all words of a srting
+ * @str: string parameter
+ *
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '.')
+		{
+			j = i + 1;
+			if (str[j] >= 97 && str[j] <= 122)
+			{
+				i = j;
+				str[i] -=32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}

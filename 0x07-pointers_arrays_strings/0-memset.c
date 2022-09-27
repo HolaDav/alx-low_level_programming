d2c3135c9c5371867e3fc09f8e6dfa89b1e3e292
HolaDav <oladejidavid12@gmail.com>
2022-09-27 00:20:43 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,8 +12,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	memset(s + i, b, n * sizeof(s));
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
 	return (s);
 }

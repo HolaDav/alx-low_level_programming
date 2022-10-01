0a0f5a1f1ff5a2e684545ce0482665fd9b8b49bb
HolaDav <oladejidavid12@gmail.com>
2022-10-01 17:44:08 +0000

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: counts arguments
+ * @argv: Holds string value of argumrnts
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) **argv;
+	printf("%d\n", argc);
+	return (0);
+}

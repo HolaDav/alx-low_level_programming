d76660b2a49144f0d385397da0e9d1e66ccec221
HolaDav <oladejidavid12@gmail.com>
2022-10-01 17:20:34 +0000

    add name

---
+++ 0x08-recursion/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of it's function
+ * @argc: Counts arguments
+ * @argv: Holds the string values of argunemts
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("The name if this file is: %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x08-recursion/0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define Main_H
+
+int _putchar (char c);
+
+#endif /** MAIN_H **/

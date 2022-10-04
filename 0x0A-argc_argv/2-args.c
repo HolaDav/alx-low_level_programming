839ca5fdf35472b1e7d7700d75956d12c2595041
HolaDav <oladejidavid12@gmail.com>
2022-10-04 14:39:06 +0000

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,5 +2,25 @@
 #include <stdio.h>
 
 /**
- * main - 
- * @a
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Stors string value of arguments
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+	return (0);
+}
